c6c81c5c4d9b50a9ab1f747425b33038ac28c4f9
anarh1st47 <anarh1st47@pm.me>
2018-11-05 10:01:55 +0300

    readme update

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## What is CrossSkelet?
 
-CrossSkelet is a cross-platform fork of Skeletux. Now it supports GNU/Linux and Windows.
+CrossSkelet is a cross-platform fork of [Skeletux](https://github.com/LWSS/Skeletux). Now it supports GNU/Linux and Windows.
 
 Skeletux is an experimental base for making your own CS:GO cheat on Linux.
-
 It is mostly aimed towards developers.
 
-
+---
 ## Features
+* Supporting both Windows and Linux
 * Fully dynamic runtime created Panorama GUI that does not touch any files and is even removed on unloading.
 * Custom ConVar settings
 * A few simple features (ESP, Bunnyhop)
@@ -19,13 +19,12 @@ It is mostly aimed towards developers.
 * A Compiler that supports C++17
 * CMake  >= 2.8
 
-===================
+
 
 #### How to Use
-
 Build like any cmake project
 
-Linux-based:
+##### Linux-based:
 ```bash
 cmake . && make 
 
@@ -36,37 +35,36 @@ Afterwards you can inject with the load script
 sudo ./load
 ```
 
-Windows-based(using VS):
+##### Windows-based(using VS):
 
 File->Open->CMake->CMakeLists.txt, than open CMakeLists file in VS and press ctrl+s, now you can build it like a standart windows project(CMake->Build all).
  
-Windows-based(console):
+##### Windows-based(console):
 TODO
 
 
 
 You can open/close the Menu with `Insert`
 
-## Screenshots
+## Screenshot
 
-This looks like garbage lol
-![menu](https://i.imgur.com/lATYWiH.jpg)
+![menu](https://i.imgur.com/XtzVXti.jpg)
 
 
 ## Thanks
-* LWSS and Fuzion contributors - Skeletux and Fuzion projects, also LWSS gives me some help in pm
-* McSwaggens, luk1337, AnterGhost, nvllvsand another AimTux contributors.
-* Atex - linux basehook project
-* aixxe - cstrike-basehook-linux project
-* MarkHC and CSGOSimple contributors - CSGOSimple project
-* vladislavik36 - some help with CrossSkelet
+
+* [LWSS](https://github.com/LWSS) and [Fuzion](https://github.com/LWSS/Fuzion) contributors - [Skeletux](https://github.com/LWSS/Skeletux) and [Fuzion](https://github.com/LWSS/Fuzion) projects, also LWSS gives me some help in pm
+* [McSwaggens](https://github.com/McSwaggens), [luk1337](https://github.com/luk1337), [AnterGhost](https://github.com/AnterGhost), [nvllvs](https://github.com/nvllvs) and another [AimTux](https://github.com/AimTuxOfficial) contributors.
+* [Atex](https://www.unknowncheats.me/forum/members/602638.html) - linux basehook project
+* [aixxe](https://github.com/aixxe) - cstrike-basehook-linux project
+* [MarkHC](https://github.com/MarkHC) and CSGOSimple contributors - CSGOSimple project
+* [vladislavik36](https://github.com/vladislavik36) - some help with CrossSkelet
 * disabilitor - some gui ideas
 
 ## TODO
 
-(todo before release)
-* Menu redisign
-* Refactoring
-
-(todo after release)
 * Mac OS support
+
+## Known Issues
+
+* [ToggleUI] - Skeletux Panel not layed out yet. Try again.
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,107 +1,108 @@
 #include "Main.h"
 
 void MainThread() {
-  Interfaces::FindInterfaces();
-  // Interfaces::DumpInterfaces();
+	Interfaces::FindInterfaces();
+	// Interfaces::DumpInterfaces();
 
-  Hooker::FindViewRender();
-  Hooker::InitializeVMHooks();
-  Hooker::FindIClientMode();
-  Hooker::FindGlobalVars();
-  Hooker::FindCInput();
-  Hooker::FindGetLocalClient();
+	Hooker::FindViewRender();
+	Hooker::InitializeVMHooks();
+	Hooker::FindIClientMode();
+	Hooker::FindGlobalVars();
+	Hooker::FindCInput();
+	Hooker::FindGetLocalClient();
 
-  if (!Settings::RegisterConVars()) {
-    cvar->ConsoleDPrintf("Error making Custom ConVars! Stopping...\n");
-    return;
-  }
+	if (!Settings::RegisterConVars()) {
+		cvar->ConsoleDPrintf("Error making Custom ConVars! Stopping...\n");
+		return;
+	}
 
-  // clientVMT->HookVM( Hooks::FrameStageNotify, 37 );
-  // clientVMT->ApplyVMT();
+	// clientVMT->HookVM( Hooks::FrameStageNotify, 37 );
+	// clientVMT->ApplyVMT();
 
-  inputInternalVMT->HookVM(Hooks::SetKeyCodeState,
-                           (int)Indexes::SetKeyCodeState);
-  inputInternalVMT->ApplyVMT();
-  clientModeVMT->HookVM((void*)Hooks::CreateMove, (int)Indexes::CreateMove);
-  clientModeVMT->ApplyVMT();
+	inputInternalVMT->HookVM(Hooks::SetKeyCodeState,
+		(int)Indexes::SetKeyCodeState);
+	inputInternalVMT->ApplyVMT();
+	clientModeVMT->HookVM((void*)Hooks::CreateMove, (int)Indexes::CreateMove);
+	clientModeVMT->ApplyVMT();
 
-  panelVMT->HookVM((void*)Hooks::PaintTraverse, (int)Indexes::PanelTraverse);
-  panelVMT->ApplyVMT();
+	panelVMT->HookVM((void*)Hooks::PaintTraverse, (int)Indexes::PanelTraverse);
+	panelVMT->ApplyVMT();
 
-  surfaceVMT->HookVM(Hooks::LockCursor, 67);
-  surfaceVMT->ApplyVMT();
+	surfaceVMT->HookVM(Hooks::LockCursor, 67);
+	surfaceVMT->ApplyVMT();
 
-  // NetVarManager::DumpNetvars();
-  Offsets::GetOffsets();
+	// NetVarManager::DumpNetvars();
+	Offsets::GetOffsets();
 
-  srand(time(NULL));  // Seed random # Generator so we can call rand() later
+	srand(time(NULL));  // Seed random # Generator so we can call rand() later
 
-  cvar->ConsoleColorPrintf(Color(0, 225, 0, 255),
-                           "\nskeletux Successfully loaded.\n");
+	cvar->ConsoleColorPrintf(Color(0, 225, 0, 255),
+		"\nskeletux Successfully loaded.\n");
 }
 
 /* Called when un-injecting the library */
-void 
+void
 #ifndef WIN32 
 __attribute__((destructor))
 #endif
 Shutdown() {
-    auto mouse = cvar->FindVar("cl_mouseenable");
-    if(mouse) mouse->SetValue(1);
+	auto mouse = cvar->FindVar("cl_mouseenable");
+	if (mouse) mouse->SetValue(1);
 
-  clientVMT->ReleaseVMT();
-  clientModeVMT->ReleaseVMT();
-  engineVGuiVMT->ReleaseVMT();
-  inputInternalVMT->ReleaseVMT();
-  launcherMgrVMT->ReleaseVMT();
-  materialVMT->ReleaseVMT();
-  surfaceVMT->ReleaseVMT();
+	clientVMT->ReleaseVMT();
+	clientModeVMT->ReleaseVMT();
+	engineVGuiVMT->ReleaseVMT();
+	inputInternalVMT->ReleaseVMT();
+	launcherMgrVMT->ReleaseVMT();
+	materialVMT->ReleaseVMT();
+	surfaceVMT->ReleaseVMT();
 
-  *s_bOverridePostProcessingDisable = false;
+	*s_bOverridePostProcessingDisable = false;
 
-  /* Cleanup ConVars we have made */
-  for (auto var : Util::createdConvars) {
-    cvar->UnregisterConCommand(var);
-  }
-  if (panoramaEngine->AccessUIEngine()->IsValidPanelPointer(GUI::skeleMain)) {
-    GUI::skeleMain->RemoveAndDeleteChildren();
-    auto parent = GUI::skeleMain->GetParent();
-    if (panoramaEngine->AccessUIEngine()->IsValidPanelPointer(parent)) {
-      parent->RemoveChild(GUI::skeleMain);
-    } else {
-      cvar->ConsoleDPrintf("ERROR unloading, panel parent is invalid!\n");
-    }
-  }
+	/* Cleanup ConVars we have made */
+	for (auto var : Util::createdConvars) {
+		cvar->UnregisterConCommand(var);
+	}
+	if (panoramaEngine->AccessUIEngine()->IsValidPanelPointer(GUI::skeleMain)) {
+		GUI::skeleMain->RemoveAndDeleteChildren();
+		auto parent = GUI::skeleMain->GetParent();
+		if (panoramaEngine->AccessUIEngine()->IsValidPanelPointer(parent)) {
+			parent->RemoveChild(GUI::skeleMain);
+		}
+		else {
+			cvar->ConsoleDPrintf("ERROR unloading, panel parent is invalid!\n");
+		}
+	}
 
-  cvar->ConsoleColorPrintf(Color(255, 0, 0, 255),
-                           "skeletux unloaded successfully.\n");
+	cvar->ConsoleColorPrintf(Color(255, 0, 0, 255),
+		"skeletux unloaded successfully.\n");
 }
 #ifndef WIN32
 /* Entrypoint to the Library. Called when loading */
 int __attribute__((constructor)) Startup() {
-  std::thread mainThread(MainThread);
-  // The root of all suffering is attachment
-  // Therefore our little buddy must detach from this realm.
-  // Farewell my thread, may we join again some day..
-  mainThread.detach();
+	std::thread mainThread(MainThread);
+	// The root of all suffering is attachment
+	// Therefore our little buddy must detach from this realm.
+	// Farewell my thread, may we join again some day..
+	mainThread.detach();
 
-  return 0;
+	return 0;
 }
 #else
 // dllmain
 BOOL WINAPI DllMain(_In_ HINSTANCE hinstDll, _In_ DWORD fdwReason,
-                    _In_opt_ LPVOID lpvReserved) {
-  switch (fdwReason) {
-    case DLL_PROCESS_ATTACH:
-      DisableThreadLibraryCalls(hinstDll);
-      CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hinstDll, 0, 0);
-      return TRUE;
-    case DLL_PROCESS_DETACH:
+	_In_opt_ LPVOID lpvReserved) {
+	switch (fdwReason) {
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hinstDll);
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hinstDll, 0, 0);
+		return TRUE;
+	case DLL_PROCESS_DETACH:
 		Shutdown();
 		return TRUE;
-      return TRUE;
-    default:
-      return TRUE;
-  }
+		return TRUE;
+	default:
+		return TRUE;
+	}
 }
 #endif
